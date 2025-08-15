591a555ec9c7c143432358e250ce18b610707ef7
tunadeveloper <tunabusiness25@gmail.com>
2025-08-15 14:52:06 +0300

    Add project files.

---
+++ BlogProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using BlogProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=TUNA\\SQLEXPRESS;Database=BlogProject-asp5;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
---
+++ BlogProject.EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.EntityLayer.Concrete
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentUsername { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+    }
+}
