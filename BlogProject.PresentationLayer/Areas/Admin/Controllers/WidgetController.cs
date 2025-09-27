8b01dbf69f2b8b6db451e7cb635dd09d5d1d8445
tunadeveloper <tunabusiness25@gmail.com>
2025-09-27 21:33:16 +0300

    Updated

---
+++ BlogProject.PresentationLayer/Areas/Admin/Controllers/WidgetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WidgetController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogProject.PresentationLayer/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.PresentationLayer.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
